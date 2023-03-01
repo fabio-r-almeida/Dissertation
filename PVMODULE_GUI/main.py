4b10ce0414a955c87d567442e4e49cc27af05dc2
Fábio Almeida <fabio-r-almeida@hotmail.com>
2023-03-01 00:00:33 +0000

    updated gui

--- PVMODULE_GUI/main.py
+++ PVMODULE_GUI/main.py
@@ -10,8 +10,9 @@ window.title(TITLE)
 frame = tkinter.Frame(window)
 frame.pack()
 
-def map_window():
 
+
+def map_window():
     map_frame = tkinter.Label(frame, text="Location Selection Map")
     map_frame.grid(row = 99 , column = 0, padx = 0, pady = 10 )
     map_widget = tkintermapview.TkinterMapView(map_frame, width=400, height=300, corner_radius=0, padx=10, pady=10)
@@ -33,41 +34,35 @@ def map_window():
         Latitude_entry_var.set(latitude)
         Longitude_entry_var.set(longitude)
         map_frame.destroy()
+
     map_widget.add_left_click_map_command(left_click_event)
         
 
     
 
-
-
-
-
-
-
-
-######Saving user info
+######Location Entry######
 user_selection_frame = tkinter.Label(frame, text="User Selection")
 user_selection_frame.grid(row = 0 , column = 0, padx = 0, pady = 10 )
-
-
 
 user_address_data = tkinter.LabelFrame(user_selection_frame, text="Location Data")
 user_address_data.grid(row = 0 , column = 0, padx = 10, pady = 10, sticky="news")
 
+######Latitude Entry######
 Latitude_label= tkinter.Label(user_address_data, text="Latitude")
 Latitude_label.grid(row = 0 , column = 0,)
-
 
 Latitude_entry_var = tkinter.StringVar(value = "00.000000")
 Latitude_Entry = tkinter.Label(user_address_data, textvariable=Latitude_entry_var)
 Latitude_Entry.grid(row = 0 , column = 1)
 
+######Longitude Entry######
 Longitude_label= tkinter.Label(user_address_data, text="Longitude")
 Longitude_label.grid(row = 1 , column = 0)
 Longitude_entry_var = tkinter.StringVar(value = "00.000000")
 Longitude_Entry = tkinter.Label(user_address_data, textvariable=Longitude_entry_var)
 Longitude_Entry.grid(row = 1 , column = 1)    
 
+######Button that opens the map######
 button = tkinter.Button(user_address_data, text = "Select From Map", command = map_window )
 button.grid(row=3, columnspan=3, sticky="news", padx=10, pady=10 )
 
@@ -75,18 +70,19 @@ button.grid(row=3, columnspan=3, sticky="news", padx=10, pady=10 )
 
 
 
-
+######Setup Entry for Modules and Inverters######
 setup = tkinter.LabelFrame(user_selection_frame, text="Setup Data")
 setup.grid(row = 0 , column = 1, padx = 10, pady = 10, sticky="news")
 
-
+######Creation of two grids side by side######
 user_panel_data = tkinter.Frame(setup)
 user_panel_data.grid(row = 0 , column = 0, padx = 10, pady = 10, sticky="news")
 
 user_inverter_data = tkinter.Frame(setup)
-user_inverter_data.grid(row = 0 , column = 1, padx = 10, pady = 10)
+user_inverter_data.grid(row = 0 , column = 1, padx = 10, pady = 10, sticky="news")
 
 
+######Population both dropdowns menus depending on the first dropdown selection######
 from pvmodule import Modules
 modules = Modules().list_modules(print_data=False)
 module_brand = modules['Manufacturer']
@@ -104,40 +100,79 @@ Module_List_Brand_Label.grid(row=0, column=0, sticky="news", padx=10, pady=10 )
 Module_List_Brand_combobox.grid(row=0, column=1, sticky="news", padx=10, pady=10 )
 
 
-
 module_model = modules['Model Number']
 Module_List_Model_Label = tkinter.Label(user_panel_data, text="Module Model")
-Module_List_Model_combobox = ttk.Combobox(user_panel_data, values= module_model.tolist())
+Module_List_Model_combobox = ttk.Combobox(user_panel_data, values= [])
 Module_List_Model_Label.grid(row=1, column=0, sticky="news", padx=10, pady=10 )
 Module_List_Model_combobox.grid(row=1, column=1, sticky="news", padx=10, pady=10 )
 
 
 
 
-Inverter_Brand_Label= tkinter.Label(user_inverter_data, text="Inverter Brand")
-Inverter_Brand_Label.grid(row = 0 , column = 0)
-Inverter_Brand_entry_var = tkinter.StringVar(value = "                                     ")
-Inverter_Brand_Entry = tkinter.Label(user_inverter_data, textvariable=Inverter_Brand_entry_var)
-Inverter_Brand_Entry.grid(row = 0 , column = 1)
 
-Inverter_Model_Label= tkinter.Label(user_inverter_data, text="Inverter Model")
-Inverter_Model_Label.grid(row = 1 , column = 0)
-Inverter_Model_entry_var = tkinter.StringVar(value = "                                     ")
-Inverter_Brand_Entry = tkinter.Label(user_inverter_data, textvariable=Inverter_Model_entry_var)
-Inverter_Brand_Entry.grid(row = 1 , column = 1)
 
 
 from pvmodule import Inverters
-inverter_list = Inverters().list_inverters()
+inverters = Inverters().list_inverters()
 
-Inverter_List_Label = tkinter.Label(user_inverter_data, text="Inverter List")
-Inverter_List_combobox = ttk.Combobox(user_inverter_data, values= inverter_list.Name.tolist())
-Inverter_List_Label.grid(row=3, column=0, sticky="news", padx=10, pady=10 )
-Inverter_List_combobox.grid(row=3, column=1, sticky="news", padx=10, pady=10 )
+inverter_brand = inverters['Manufacturer']
+
+inverter_brand = list(dict.fromkeys(inverter_brand.tolist()))
+v = []
+def combofill(event):
+    v = inverters[ inverters['Manufacturer'] == Inverter_List_Brand_combobox.get()]['Model Number'].tolist()
+    Inverter_List_Model_combobox.config(values=v)
+
+Inverter_List_Brand_Label = tkinter.Label(user_inverter_data, text="Inverter Brand")
+Inverter_List_Brand_combobox = ttk.Combobox(user_inverter_data, values= inverter_brand)
+Inverter_List_Brand_combobox.bind('<<ComboboxSelected>>', combofill)
+Inverter_List_Brand_Label.grid(row=0, column=0, sticky="news", padx=10, pady=10 )
+Inverter_List_Brand_combobox.grid(row=0, column=1, sticky="news", padx=10, pady=10 )
+
+
+inverter_model = inverters['Model Number']
+Inverter_List_Model_Label = tkinter.Label(user_inverter_data, text="Inverter Model")
+Inverter_List_Model_combobox = ttk.Combobox(user_inverter_data, values= [])
+Inverter_List_Model_Label.grid(row=1, column=0, sticky="news", padx=10, pady=10 )
+Inverter_List_Model_combobox.grid(row=1, column=1, sticky="news", padx=10, pady=10 )
+
+
+user_panel_datasheet = tkinter.Frame(setup)
+user_panel_datasheet.grid(row = 1 , column = 0, padx = 10, pady = 10, sticky="news")
+
+user_inverter_datasheet = tkinter.Frame(setup)
+user_inverter_datasheet.grid(row = 1 , column = 1, padx = 10, pady = 10, sticky="news")
+
+
+
+######Aditional information regarding the selection above######
+Module_Peak_Wattage_Label= tkinter.Label(user_panel_datasheet, text="Module Peak Wattage")
+Module_Peak_Wattage_Label.grid(row = 0 , column = 0, sticky="news", padx=10, pady=10 )
+Module_Peak_Wattage_entry_var = tkinter.StringVar(value = "0")
+Module_Peak_Wattage_Entry = tkinter.Label(user_panel_datasheet, textvariable=Module_Peak_Wattage_entry_var)
+Module_Peak_Wattage_Entry.grid(row = 0 , column = 1, sticky="news", padx=10, pady=10 ) 
+
+Module_Technology_Label= tkinter.Label(user_panel_datasheet, text="Module Technology")
+Module_Technology_Label.grid(row = 1 , column = 0, sticky="news", padx=10, pady=10 )
+Module_Technology_entry_var = tkinter.StringVar(value = "0")
+Module_Technology_Entry = tkinter.Label(user_panel_datasheet, textvariable=Module_Technology_entry_var)
+Module_Technology_Entry.grid(row = 1 , column = 1, sticky="news", padx=10, pady=10 ) 
+
+Module_Bifaciality_Label= tkinter.Label(user_panel_datasheet, text="Module Bifaciality")
+Module_Bifaciality_Label.grid(row = 2 , column = 0, sticky="news", padx=10, pady=10 )
+Module_Bifaciality_entry_var = tkinter.StringVar(value = "0")
+Module_Bifaciality_Entry = tkinter.Label(user_panel_datasheet, textvariable=Module_Bifaciality_entry_var)
+Module_Bifaciality_Entry.grid(row = 2 , column = 1, sticky="news", padx=10, pady=10 ) 
 
 
 
 
+
+
+
+
+
+######Automatically adding padding to all children######
 for widget in user_address_data.winfo_children():
     widget.grid_configure(padx = 10, pady = 5)
 
@@ -151,6 +186,9 @@ for widget in user_panel_data.winfo_children():
     widget.grid_configure(padx = 10, pady = 5)
 
 for widget in user_inverter_data.winfo_children():
+    widget.grid_configure(padx = 10, pady = 5)
+
+for widget in setup.winfo_children():
     widget.grid_configure(padx = 10, pady = 5)
 
 
