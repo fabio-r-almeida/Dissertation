ccf1a3749c6bb3f6c44e4f931a7e020470664e3a
unknown <fabio-r-almeida@hotmail.com>
2023-04-22 13:02:34 +0100

    Update changelog.py

--- PVMODULE_GUI/changelog.py
+++ PVMODULE_GUI/changelog.py
@@ -1,12 +1,12 @@
-### [0.0.66] to [0.0.130] - 2023-03-20
-### Added
+
+\033[1m Added \033[0m
 - Added new *Graph* class.
 - Multithreading yearly horizontal and vertical data acquisition with 
   - *PVGIS().retrieve_all_year_bifacial()*
   - *PVGIS().retrieve_all_year()*
-### Fixed
+\033[1m Fixed \033[0m
 - Improved inverter auto-selection.
 - Added error exception in both *Inverter* and *PVGIS* class.
-### Removed
+\033[1m Removed \033[0m
 - Irradiance class will soon be removed due to incorrect irradiance estimations.
   - This issue is believed to be cause due to the incorrect shadow calculation of the module.
