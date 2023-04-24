e97efb502cb4365834aedffe9151a4ba95dd04d0
unknown <fabio-r-almeida@hotmail.com>
2023-04-24 09:08:38 +0100

    Automatic Push

--- PVMODULE_GUI/Executable.bat
+++ PVMODULE_GUI/Executable.bat
@@ -11,6 +11,7 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
      )
 
 set datestr=__version__ = '%year%.%month%.
+set verstr=%year%.%month%.
 
 @echo off
 
@@ -22,11 +23,24 @@ set datestr=__version__ = '%year%.%month%.
         >>"%textFile%" echo(%datestr%%replace%'!
         endlocal
     )
+@echo off
+
+    set "textFile=VERSION_ini.txt"
+
+    for /f "delims=" %%i in ('type "%textFile%" ^& break ^> "%textFile%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile%" echo(%verstr%%replace%!
+        endlocal
+    )
 
 <NUL set /p=%COUNTER%> 1_current_version.txt
 copy VERSION.txt version.py
+copy VERSION_ini.txt config.ini
 
-pyinstaller --noconsole --noconfirm --onedir --windowed --clean --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/VERSION.txt;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/Plot.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/Map.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/Splash.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/Loading.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/Get_Data_Threads.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/images;images/" --add-data "C:/Users/Fabio/AppData/Local/Programs/Python/Python38/Lib/site-packages/customtkinter;customtkinter/" --icon "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/images/icon.ico" "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/PVModule.py" 
+pyinstaller --noconsole --noconfirm --onedir --windowed --clean --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/PPFD.Plot.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/VERSION.txt;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/Plot.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/Map.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/Splash.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/Loading.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/Get_Data_Threads.py;." --add-data "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/images;images/" --add-data "C:/Users/Fabio/AppData/Local/Programs/Python/Python38/Lib/site-packages/customtkinter;customtkinter/" --icon "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/images/icon.ico" "C:/Users/Fabio/Desktop/Github/Dissertation/PVMODULE_GUI/PVModule.py" 
+
+issc inno_setup.iss
 
 git add -A
 git commit -m "Automatic Push"
